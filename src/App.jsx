a0a8631f31f56d41f44d84e07ede80f499967a97
yu wang <nightlight0125@gmail.com>
2025-11-23 11:22:33 +0800

    water mark

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Watermark from "./components/Watermark";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Accounts from "./pages/accounts/Accounts";
 import AccountDetail from "./pages/accounts/AccountDetail";
@@ -10,22 +11,25 @@ import TwoFactorAuthentication from "./pages/twofactor/TwoFactorAuthentication";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/accounts" element={<Accounts />} />
-        <Route path="/accounts/detail" element={<AccountDetail />} />
-        <Route path="/pay-transfer" element={<PayTransfer />} />
-        <Route path="/customer-insights" element={<CustomerInsights />} />
-        <Route path="/statements" element={<Statements />} />
-        <Route path="/security" element={<Security />} />
-        <Route
-          path="/two-factor-authentication"
-          element={<TwoFactorAuthentication />}
-        />
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/accounts" element={<Accounts />} />
+          <Route path="/accounts/detail" element={<AccountDetail />} />
+          <Route path="/pay-transfer" element={<PayTransfer />} />
+          <Route path="/customer-insights" element={<CustomerInsights />} />
+          <Route path="/statements" element={<Statements />} />
+          <Route path="/security" element={<Security />} />
+          <Route
+            path="/two-factor-authentication"
+            element={<TwoFactorAuthentication />}
+          />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+      <Watermark text="For Internal Use Only" opacity={0.08} angle={-22} gap={140} size={16} />
+    </>
   );
 }
 
